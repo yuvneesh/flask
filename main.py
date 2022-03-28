9b0a64ffcf785a2f12a90eb33ce0ffcabf56f452
Yuvneesh Kashyap <yuvneeshk53@gmail.com>
2022-03-28 01:46:26 -0400

    Added a date page to show current date

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from flask import Flask
+import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def main_function():
     return 'This is a main function, but modified'
+
+@app.route('/date')
+def printDate():
+    x = datetime.datetime.now()
+    x = x.strftime("%B %d, %Y")
+    return x
+
+#Alternate to app.route decorator is the app.add_url_rule function. Both of these were tested succesfully here.
+#app.add_url_rule('/date', view_func=printDate)
 
 if __name__ == '__main__':
     app.run(debug=True)
